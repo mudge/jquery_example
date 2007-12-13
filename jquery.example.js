d7b1ecaebf3cd128e3502bfea689b1251d9c722f
Paul Mucur <mudge@mudge.name>
2007-12-13 14:02:16 +0000

    Allows for values the same as example text and only fills empty fields on load.
    Improved the conditions when inserting example text and when clearing fields for
    entry. Instead of just checking whether the value of a field matches the example
    text given on focus, the class of the field is also checked to more reliably
    determine whether it is an example or not.
    
    This means that example texts are now valid possible values whereas previously such
    values would have been cleared. e.g. If the example for a field is "Bob Smith",
    putting "Bob Smith" as the actual value will now work.
    
    In the same vein, examples are only set on page load if the selected fields
    are empty. Previously, examples would always overwrite existing field values.

--- jquery.example.js
+++ jquery.example.js
@@ -30,19 +30,26 @@
 				}
 			});
 			
-			/* Initially place the example text in the field. */
-			$this.val(text);
-			$this.addClass(options.class_name);
-
+			/* Initially place the example text in the field if it is empty. */
+			if ($this.val() == '') {
+				$this.val(text);
+				$this.addClass(options.class_name);
+			}
+		
 			/* If the option is set, remove the associated label (and its line-break if it has one). */
 			if (options.remove_label) {
 				$('label[@for=' + $this.attr('id') + ']').next('br').remove();
 				$('label[@for=' + $this.attr('id') + ']').remove();
 			}
 		
-			/* Make the example text disappear when someone focuses. */
+			/* Make the example text disappear when someone focuses.
+			 *
+			 * To determine whether the value of the field is an example or not,
+			 * use both the sample text AND the example class name as someone
+			 * may choose to use the sample text as a real value.
+			 */
 			$this.focus(function() {
-				if ($(this).val() == text) {
+				if ($(this).val() == text && $(this).hasClass(options.class_name)) {
 					$(this).val('');
 					$(this).removeClass(options.class_name);
 				}
