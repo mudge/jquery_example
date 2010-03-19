26e88d7f4a09038d65739ee4c6c06694f28f2423
Matt Buck <techpeace@gmail.com>
2010-03-20 05:05:29 +0800

    Added selectAll option to select example text on focus, rather than clear it out. Closes #1
    Squashed commit of the following:
    
    commit e4ea5189bb237a0f76d74316ce84c4efb1244d70
    Author: Matt Buck <techpeace@gmail.com>
    Date:   Fri Mar 19 16:00:05 2010 -0500
    
        Re-minified library.
    
    commit bffe67c95f5f8b3a205d37cb9e2ef5ca2827f6f5
    Author: Matt Buck <techpeace@gmail.com>
    Date:   Fri Mar 19 15:59:37 2010 -0500
    
        Added test to cover new functionality.
    
    commit 3e3ce8f3999c9c95de6fdebffd19ebca45fb6cd7
    Author: Matt Buck <techpeace@gmail.com>
    Date:   Fri Mar 19 15:38:05 2010 -0500
    
        Removing more whitespace.
    
    commit 3db3c2c396266dae5c2184511007945de2c2205d
    Author: Matt Buck <techpeace@gmail.com>
    Date:   Mon Mar 15 17:19:32 2010 -0500
    
        Added selectAll option.
    
    commit 048a1e133764e8a700fc40e0917aed9ee167d41c
    Author: Matt Buck <techpeace@gmail.com>
    Date:   Mon Mar 15 15:52:06 2010 -0500
    
        Added .swp files to gitignore.
    
    commit 31e8721faade592cf70dad80e01476025e3e0de2
    Author: Matt Buck <techpeace@gmail.com>
    Date:   Mon Mar 15 15:51:59 2010 -0500
    
        Remove trailing whitespace.

--- jquery.example.js
+++ jquery.example.js
@@ -26,29 +26,29 @@
  * GNU General Public License for more details.
  */
 (function($) {
-  
+
   $.fn.example = function(text, args) {
-    
+
     /* Only calculate once whether a callback has been used. */
     var isCallback = $.isFunction(text);
-    
+
     /* Merge the arguments and given example text into one options object. */
     var options = $.extend({}, args, {example: text});
-    
+
     return this.each(function() {
-      
+
       /* Reduce method calls by saving the current jQuery object. */
       var $this = $(this);
-      
+
       /* Merge the plugin defaults with the given options and, if present,
        * any metadata.
        */
-      if ($.metadata) {        
+      if ($.metadata) {
         var o = $.extend({}, $.fn.example.defaults, $this.metadata(), options);
       } else {
         var o = $.extend({}, $.fn.example.defaults, options);
       }
-      
+
       /* The following event handlers only need to be bound once
        * per class name. In order to do this, an array of used
        * class names is stored and checked on each use of the plugin.
@@ -61,7 +61,7 @@
        * $.inArray().
        */
       if (!$.fn.example.boundClassNames[o.className]) {
-      
+
         /* Because Gecko-based browsers cache form values
          * but ignore all other attributes such as class, all example
          * values must be cleared on page unload to prevent them from
@@ -70,7 +70,7 @@
         $(window).unload(function() {
           $('.' + o.className).val('');
         });
-      
+
         /* Clear fields that are still examples before any form is submitted
          * otherwise those examples will be sent along as well.
          * 
@@ -79,15 +79,15 @@
          * multiple forms would not work correctly.
          */
         $('form').submit(function() {
-        
+
           /* Clear only the fields inside this particular form. */
           $(this).find('.' + o.className).val('');
         });
-      
+
         /* Add the class name to the array. */
         $.fn.example.boundClassNames[o.className] = true;
       }
-    
+
       /* Internet Explorer will cache form values even if they are cleared
        * on unload, so this will clear any value that matches the example
        * text and hasn't been specified in the value attribute.
@@ -97,23 +97,25 @@
        * are cleared. This means that caching is effectively disabled for
        * that field.
        *
+       * We will not clear default values if the selectAll option is true.
+       *
        * Many thanks to Klaus Hartl for helping resolve this issue.
        */
-      if ($.browser.msie && !$this.attr('defaultValue') && (isCallback || $this.val() == o.example))
+      if ($.browser.msie && !$this.attr('defaultValue') && (isCallback || $this.val() == o.example) && !o.selectAll)
         $this.val('');
-      
+
       /* Initially place the example text in the field if it is empty
        * and doesn't have focus yet.
        */
       if ($this.val() == '' && this != document.activeElement) {
         $this.addClass(o.className);
-        
+
         /* The text argument can now be a function; if this is the case,
          * call it, passing the current element as `this`.
          */
         $this.val(isCallback ? o.example.call(this) : o.example);
       }
-      
+
       /* Make the example text disappear when someone focuses.
        *
        * To determine whether the value of the field is an example or not,
@@ -122,11 +124,16 @@
        * input.
        */
       $this.focus(function() {
-        
+
         /* jQuery 1.1 has no hasClass(), so is() must be used instead. */
         if ($(this).is('.' + o.className)) {
-          $(this).val('');
           $(this).removeClass(o.className);
+
+          if (o.selectAll) {
+            $(this).select();
+          } else {
+            $(this).val('');
+          }
         }
       });
 
@@ -141,7 +148,7 @@
       $this.blur(function() {
         if ($(this).val() == '') {
           $(this).addClass(o.className);
-          
+
           /* Re-evaluate the callback function every time the user
            * blurs the field without entering anything. While this
            * is not as efficient as caching the value, it allows for
@@ -152,16 +159,17 @@
       });
     });
   };
-  
+
   /* Users can override the defaults for the plugin like so:
    *
    *   $.fn.example.defaults.className = 'not_example';
    */
   $.fn.example.defaults = {
-    className: 'example'
+    className: 'example',
+    selectAll: false
   };
-  
+
   /* All the class names used are stored as keys in the following array. */
   $.fn.example.boundClassNames = [];
-  
+
 })(jQuery);
