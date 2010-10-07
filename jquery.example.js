64f44335e6ec1d64c99c526513aa4fc1901ebe90
Paul Mucur <mudge@mudge.name>
2010-10-07 22:33:36 +0100

    Add QUnit test suite.

--- jquery.example.js
+++ jquery.example.js
@@ -9,8 +9,8 @@
 
   $.fn.example = function(text) {
     return this.each(function() {
-      var exampleText = $.isFunction(text) ? text.call(this) : text;
       var field = $(this);
+      var exampleText = $.isFunction(text) ? text.call(this) : text;
       var example = $('<span>' + exampleText + '</span>').insertAfter(field);
 
       /* Retain the original background of the element so it can
@@ -26,36 +26,47 @@
         'background-repeat':      field.css('background-repeat')
       };
 
-      var addBorderWidthToMargin = function(side) {
-        return function(index, value) {
-          return parseFloat(value) +
-              parseFloat(field.css('border-' + side + '-width'));
-        }
-      };
+      // Copy styling from the field to the example.
+      copyCSS(field, example, [
 
-      // Copy the background and font styling.
-      copyCSS(field, example, ['background-image', 'background-attachment',
-          'background-origin', 'background-clip', 'background-color',
-          'background-position', 'background-repeat', 'font-style', 'font-variant',
-          'font-weight', 'font-size', 'line-height', 'font-family',
-          'border-top-left-radius', 'border-top-right-radius',
-          'border-bottom-right-radius', 'border-bottom-left-radius',
-          '-moz-border-radius-topleft', '-moz-border-radius-topright',
-          '-moz-border-radius-bottomleft', '-moz-border-radius-bottomright',
-          'width', 'height', 'padding-top', 'padding-right',
-          'padding-bottom', 'padding-left', 'margin-top', 'margin-left',
-          'margin-bottom', 'margin-right']);
+        // Background.
+        'background-image', 'background-attachment', 'background-origin',
+        'background-clip', 'background-color', 'background-position',
+        'background-repeat',
+
+        // Font styling.
+        'font-style', 'font-variant', 'font-weight', 'font-size', 'line-height',
+        'font-family',
+
+        // Borders.
+        'border-top-left-radius', 'border-top-right-radius',
+        'border-bottom-right-radius', 'border-bottom-left-radius',
+        '-moz-border-radius-topleft', '-moz-border-radius-topright',
+        '-moz-border-radius-bottomleft', '-moz-border-radius-bottomright',
+        'border-top-width', 'border-right-width', 'border-bottom-width',
+        'border-left-width', 'border-top-style', 'border-right-style',
+        'border-bottom-style', 'border-left-style',
+
+        // Padding.
+        'padding-top', 'padding-bottom',
+
+        // Dimensions.
+        'height',
+
+        // Margins.
+        'margin-top', 'margin-left', 'margin-bottom', 'margin-right'
+      ]);
 
       example.css({
         'display':        'inline-block',
         'position':       'relative',
-        'left':           field.position().left - example.position().left,
-        'margin-top':     addBorderWidthToMargin('top'),
-        'margin-right':   addBorderWidthToMargin('right'),
-        'margin-bottom':  addBorderWidthToMargin('bottom'),
-        'margin-left':    addBorderWidthToMargin('left'),
+        'border-color':   'transparent',
+        'width':          field.width() - 2,
+        'left':           field.offset().left - example.offset().left,
+        'padding-right':  parseInt(field.css('padding-right'), 10) + 1,
+        'padding-left':   parseInt(field.css('padding-left'), 10) + 1,
         'z-index':        function(index, value) {
-          var currentIndex = parseFloat(field.css('z-index'));
+          var currentIndex = parseInt(field.css('z-index'), 10);
           if (isNaN(currentIndex)) {
             return -1;
           } else {
