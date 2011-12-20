a61b94e5af541d3d9c75c289e3260e83f64c10c2
Paul Mucur <mudge@mudge.name>
2011-12-20 18:25:28 +0000

    Update to 1.5.2.

---
+++ jquery.example.js
@@ -0,0 +1,166 @@
+/*global window, document, jQuery */
+
+/*
+ * jQuery Form Example Plugin 1.5.2
+ * Populate form inputs with example text that disappears on focus.
+ *
+ * e.g.
+ *    $('input#name').example('Bob Smith');
+ *    $('input[@title]').example(function () {
+ *        return $(this).attr('title');
+ *    });
+ *    $('textarea#message').example('Type your message here', {
+ *        className: 'example_text'
+ *    });
+ *
+ * Copyright (c) Paul Mucur (http://mudge.name), 2007-2011.
+ * Dual-licensed under the BSD (BSD-LICENSE.txt) and GPL (GPL-LICENSE.txt)
+ * licenses.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+ * GNU General Public License for more details.
+ */
+(function ($) {
+    "use strict";
+
+    $.fn.example = function (text, args) {
+
+        var isCallback = $.isFunction(text),
+            options    = $.extend({}, args, {example: text});
+
+        return this.each(function () {
+
+            var o, clearExamples, $this = $(this);
+
+            /* Merge the plugin defaults with the given options and, if present,
+             * any metadata.
+             */
+            if ($.metadata) {
+                o = $.extend({}, $.fn.example.defaults, $this.metadata(), options);
+            } else {
+                o = $.extend({}, $.fn.example.defaults, options);
+            }
+
+            clearExamples = function () {
+                $(this).find('.' + o.className).val('');
+            };
+
+            /* The following event handlers only need to be bound once
+             * per class name. In order to do this, an array of used
+             * class names is stored and checked on each use of the plugin.
+             * If the class name is in the array then this whole section
+             * is skipped. If not, the events are bound and the class name
+             * added to the array.
+             */
+            if (!$.fn.example.boundClassNames[o.className]) {
+
+                /* Because Gecko-based browsers cache form values
+                 * but ignore all other attributes such as class, all example
+                 * values must be cleared on page unload to prevent them from
+                 * being saved.
+                 */
+                $(window).bind('unload.example', function () {
+                    $('.' + o.className).val('');
+                });
+
+                /* Clear fields that are still examples before any form is submitted
+                 * otherwise those examples will be sent along as well.
+                 *
+                 * Where possible, attempt to delegate this event handler so that
+                 * all forms are bound now and in the future.
+                 */
+                if ($.fn.on) {
+                    $('body').on('submit.example', 'form', clearExamples);
+                } else if ($.fn.delegate) {
+                    $('body').delegate('form', 'submit.example', clearExamples);
+                } else if ($.fn.live) {
+                    $('form').live('submit.example', clearExamples);
+                } else {
+                    $('form').bind('submit.example', clearExamples);
+                }
+
+                /* Add the class name to the array. */
+                $.fn.example.boundClassNames[o.className] = true;
+            }
+
+            /* Several browsers will cache form values even if they are cleared
+             * on unload, so this will clear any value that matches the example
+             * text and hasn't been specified in the value attribute.
+             *
+             * If a callback is used, it is not possible or safe to predict
+             * what the example text is going to be so all non-default values
+             * are cleared. This means that caching is effectively disabled for
+             * that field.
+             *
+             * Many thanks to Klaus Hartl for helping resolve this issue.
+             */
+            if ($this.val() !== this.defaultValue && (isCallback || $this.val() === o.example)) {
+                $this.val(this.defaultValue);
+            }
+
+            /* Initially place the example text in the field if it is empty
+             * and doesn't have focus yet.
+             */
+            if ($this.val() === '' &&
+                (!document.hasOwnProperty('activeElement') || this !== document.activeElement)) {
+
+                /* The text argument can now be a function; if this is the case,
+                 * call it, passing the current element as `this`.
+                 */
+                $this.addClass(o.className).val(isCallback ? o.example.call(this) : o.example);
+            }
+
+            /* Make the example text disappear when someone focuses.
+             *
+             * To determine whether the value of the field is an example or not,
+             * check for the example class name only; comparing the actual value
+             * seems wasteful and can stop people from using example values as real
+             * input.
+             */
+            $this.bind('focus.example', function () {
+
+                if ($(this).is('.' + o.className)) {
+                    $(this).val('').removeClass(o.className);
+                }
+
+            }).bind('change.example', function () {
+
+                if ($(this).is('.' + o.className)) {
+                    $(this).removeClass(o.className);
+                }
+
+            }).bind('blur.example', function () {
+
+                if ($(this).val() === '') {
+
+                    /* Re-evaluate the callback function every time the user
+                     * blurs the field without entering anything. While this
+                     * is not as efficient as caching the value, it allows for
+                     * more dynamic applications of the plugin.
+                     */
+                    $(this).addClass(o.className).val(isCallback ? o.example.call(this) : o.example);
+                }
+
+            });
+        });
+    };
+
+    /* Users can override the defaults for the plugin like so:
+     *
+     *     $.fn.example.defaults.className = 'not_example';
+     */
+    $.fn.example.defaults = {
+        className: 'example'
+    };
+
+    /* All the class names used are stored as keys in the following array. */
+    $.fn.example.boundClassNames = [];
+
+}(jQuery));
---
+++ test/jquery.example_test.js
@@ -0,0 +1,263 @@
+/*global module, test, equal, ok, jQuery */
+(function ($) {
+    "use strict";
+
+    /* If there is no delegation support, forcibly reset the plugin between
+     * test runs
+     */
+    function resetPlugin() {
+        if (!$.fn.on && !$.fn.delegate && !$.fn.live) {
+            $.fn.example.boundClassNames = [];
+        }
+    }
+
+    module("Basic usage", {
+        setup: function () {
+            $('#basic1').example('Test');
+            $('#basicform').submit(function (e) {
+                e.preventDefault();
+            });
+        },
+        teardown: resetPlugin
+    });
+    test("should have an example set", function () {
+        equal($('#basic1').val(), "Test", "The example should read 'Test'.");
+        ok($('#basic1').hasClass('example'), "The class should be 'example'.");
+    });
+    test("should be cleared on focus", function () {
+        $('#basic1').focus();
+
+        equal($('#basic1').val(), "", "The example should be cleared.");
+        ok(!$('#basic1').hasClass('example'), "The class should no longer be 'example'.");
+    });
+    test("should reappear on blur if empty", function () {
+        $('#basic1').focus().blur();
+
+        equal($('#basic1').val(), "Test", "The example should read 'Test'.");
+        ok($('#basic1').hasClass('example'), "The class should be 'example'.");
+    });
+    test("should not be populated with an example on blur if user input is present", function () {
+        $('#basic1').focus();
+        $('#basic1').val("My own value");
+        $('#basic1').blur();
+
+        equal($('#basic1').val(), "My own value", "The example should not be cleared.");
+        ok(!$('#basic1').hasClass('example'), "The class should not be 'example'.");
+    });
+    test("should not be populated with an example on focus if user input is present", function () {
+        $('#basic1').focus().val("My own value").blur().focus();
+
+        equal($('#basic1').val(), "My own value", "The example should not be cleared.");
+        ok(!$('#basic1').hasClass('example'), "The class should not be 'example'.");
+    });
+    test("should be cleared on form submit", function () {
+        $('#basicform').submit();
+
+        equal($('#basic1').val(), "", "The example should be cleared.");
+    });
+    test("shouldn't clear user inputs on form submit", function () {
+        $('#basic2').focus().val("User input");
+        $('#basicform').triggerHandler('submit');
+
+        equal($('#basic2').val(), "User input", "The user input should be intact.");
+    });
+
+    module("Using custom classes", {
+        setup: function () {
+            $('#custom1').example("Test", {className: "notExample"});
+        },
+        teardown: resetPlugin
+    });
+    test("should have an example set", function () {
+        equal($('#custom1').val(), "Test", "The example should be set.");
+        ok($('#custom1').hasClass('notExample'), "The class should be the specified one.");
+        ok(!$('#custom1').hasClass('example'), "The class should not be 'example'.");
+    });
+    test("should be cleared on focus", function () {
+        $('#custom1').focus();
+
+        equal($('#custom1').val(), "", "The example should be cleared.");
+        ok(!$('#custom1').hasClass('notExample'), "The class should not be the specified one.");
+    });
+    test("should be reappear on blur", function () {
+        $('#custom1').focus().blur();
+
+        equal($('#custom1').val(), "Test", "The example should reappear.");
+        ok($('#custom1').hasClass('notExample'), "The class should be the specified one.");
+    });
+
+    module("Multiple forms", {
+        setup: function () {
+            $('#multipleform1, #multipleform2').submit(function (e) {
+                e.preventDefault();
+            });
+            $('#mf1').example('Test');
+            $('#mf2').example('Test');
+        },
+        teardown: resetPlugin
+    });
+    test("should only clear examples in that form", function () {
+        $('#multipleform1').submit();
+
+        equal($('#mf1').val(), "", "The example should be cleared.");
+        equal($('#mf2').val(), "Test", "An example in another form should not be cleared.");
+    });
+
+    module("Simple callback", {
+        setup: function () {
+            $('#callback1').example(function () { return "Callback Test"; });
+        },
+        teardown: resetPlugin
+    });
+    test("should have an example set", function () {
+        equal($('#callback1').val(), "Callback Test", "The example should read 'Callback Test'.");
+        ok($('#callback1').hasClass('example'), "The class should be 'example'.");
+    });
+    test("should be cleared on focus", function () {
+        $('#callback1').focus();
+
+        equal($('#callback1').val(), "", "The example should be cleared.");
+        ok(!$('#callback1').hasClass('example'), "The class should no longer be 'example'.");
+    });
+    test("should reappear on blur if empty", function () {
+        $('#callback1').focus().blur();
+
+        equal($('#callback1').val(), "Callback Test", "The example should read 'Callback Test'.");
+        ok($('#callback1').hasClass('example'), "The class should be 'example'.");
+    });
+
+    module("More complicated callback", {
+        setup: function () {
+            $('#callback2').example(function () {
+                return $(this).attr('title');
+            });
+        },
+        teardown: resetPlugin
+    });
+    test("should have an example set", function () {
+        equal($('#callback2').val(), "Starting", "The example should read 'Starting'.");
+        ok($('#callback2').hasClass('example'), "The class should be 'example'.");
+    });
+    test("should be cleared on focus", function () {
+        $('#callback2').focus();
+
+        equal($('#callback2').val(), "", "The example should be cleared.");
+        ok(!$('#callback2').hasClass('example'), "The class should no longer be 'example'.");
+    });
+    test("should reappear on blur if empty", function () {
+        $('#callback2').focus().blur();
+
+        equal($('#callback2').val(), "Starting", "The example should read 'Starting'.");
+        ok($('#callback2').hasClass('example'), "The class should be 'example'.");
+    });
+    test("should run the callback every time instead of caching it", function () {
+        $('#callback2').attr('title', 'Another');
+        $('#callback2').focus().blur();
+
+        equal($('#callback2').val(), "Another", "The example should read 'Another'.");
+        ok($('#callback2').hasClass('example'), "The class should be 'example'.");
+    });
+
+    module("Metadata plugin", {
+        setup: function () {
+            $('#m1').example();
+        },
+        teardown: resetPlugin
+    });
+    test("should have an example set", function () {
+        equal($('#m1').val(), "Something", "The example should read 'Something'.");
+        ok($('#m1').hasClass('m1'), "The class should be 'm1'.");
+    });
+    test("should be cleared on focus", function () {
+        $('#m1').focus();
+
+        equal($('#m1').val(), "", "The example should be cleared.");
+        ok(!$('#m1').hasClass('m1'), "The class should no longer be 'm1'.");
+    });
+    test("should reappear on blur if empty", function () {
+        $('#m1').focus().blur();
+
+        equal($('#m1').val(), "Something", "The example should read 'Something'.");
+        ok($('#m1').hasClass('m1'), "The class should be 'm1'.");
+    });
+    test("should be overridden by arguments", function () {
+        $('#m2').example('Precedence', {className: 'o1'});
+
+        equal($('#m2').val(), "Precedence", "The example in the arguments should take precedence");
+        ok($('#m2').hasClass('o1'), "The class should be 'o1'.");
+    });
+
+    module("On page load", {
+        teardown: resetPlugin
+    });
+    test("should not set an example if a value is already set", function () {
+        $('#load1').example("Test");
+
+        equal($('#load1').val(), "Already filled in", "The example should not be set.");
+        ok(!$('#load1').hasClass('example'), "The class should not be 'example'.");
+    });
+    test("should not clear a field with a value even when using a callback", function () {
+        $('#load2').example(function () {
+            return "Nope";
+        });
+
+        equal($('#load2').val(), "Default", "The value should be the default.");
+        ok(!$('#load2').hasClass('example'), "The class should not be 'example'.");
+    });
+
+    module("Changing values by Javascript", {
+        setup: function () {
+            $('#f1').example('Example');
+        },
+        teardown: resetPlugin
+    });
+    test("should set example", function () {
+        equal($('#f1').val(), "Example", "The example should read 'Example'.");
+        ok($('#f1').hasClass('example'), "The example class should be set.");
+    });
+
+    test("should remove example class when changed", function () {
+        $('#f1').val("New value");
+        $('#f1').change();
+
+        equal($('#f1').val(), "New value", "Value should be changed to 'New value'.");
+        ok(!$('#f1').hasClass('example'), "The example class should no longer be set.");
+
+        /* Clear the field between test runs. */
+        $('#f1').val('');
+    });
+
+    module("Clearing values when loaded from cache", {
+        teardown: resetPlugin
+    });
+    test("value should be set to default value", function () {
+
+        /* Fake loading from cache by setting the example to be different to
+         * the recorded defaultValue.
+         */
+        $('#c1').val('Cached example').example('Cached example');
+        equal($('#c1').val(), "Filled in", "Value should have been reset to 'Filled in'.");
+    });
+    test("value should be cleared and set to the example if without default", function () {
+        $('#c2').val('Cached example').example('Cached example');
+        equal($('#c2').val(), 'Cached example', "Value should have been emptied.");
+        ok($('#c2').hasClass('example'), 'The example class should be set.');
+    });
+    test("value is not touched if it doesn't match the example", function () {
+        $('#c3').val('Some user input').example('Test');
+        equals($('#c3').val(), 'Some user input', 'Value should not have been modified.');
+        ok(!$('#c3').hasClass('example'), 'The example class should not be set.');
+    });
+    test('value is always cleared if the example is a callback', function () {
+        $('#c4').val('Some user input').example(function () {
+            return 'Test';
+        });
+        equals($('#c4').val(), 'Test', 'The cached value is overridden.');
+        ok($('#c4').hasClass('example'), 'The example class should be set.');
+    });
+    test("value is not touched if it is the default", function () {
+        $('#c5').val('Some default').example('Test');
+        equals($('#c5').val(), 'Some default', 'Value should not have been modified.');
+        ok(!$('#c5').hasClass('example'), 'The example class should not be set.');
+    });
+}(jQuery));
